165b049c30e4de00bfaf444808ffc05cc55d7763
admin <bosko15082004@gmail.com>
2025-02-02 23:57:51 +0300

    Trying to configure security

---
+++ auth-service/src/main/java/org/example/controllers/AuthController.java
@@ -0,0 +1,58 @@
+package org.example.controllers;
+
+import org.example.dto.AuthRequest;
+import org.example.dto.AuthResponse;
+import org.example.dto.RegistrationRequest;
+import org.example.entities.Users;
+import org.example.security.CustomUserDetails;
+import org.example.security.JwtUtil;
+import org.example.services.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody AuthRequest authRequest) {
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
+            );
+            CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+            String token = jwtUtil.generateToken(userDetails);
+            return ResponseEntity.ok(new AuthResponse(token));
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+        }
+    }
+
+//    @PostMapping("/register")
+//    public ResponseEntity<?> register(@RequestBody RegistrationRequest request) {
+//        try {
+//            Users newUser = usersService.registerNewUser(request.getUsername(), request.getPassword(), request.getRole());
+//            return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+//        } catch (RuntimeException e) {
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+//        }
+//    }
+}
---
+++ auth-service/src/main/java/org/example/services/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package org.example.services;
+
+import org.example.entities.Users;
+import org.example.repositories.UsersRepository;
+import org.example.security.CustomUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users user = usersRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new CustomUserDetails(user);
+    }
+}
